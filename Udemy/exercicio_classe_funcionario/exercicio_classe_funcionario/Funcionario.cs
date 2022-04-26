442d98c338e27170e8db199a26dae48e89af0185
Diego Gomes <diegogns18@outlook.com>
2022-04-26 18:14:21 -0300

    exercicio_classe_funcionario

---
+++ Udemy/exercicio_classe_funcionario/exercicio_classe_funcionario/Funcionario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_classe_funcionario
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
