2ad7360b070318886cc72b2f58ed711e774af98b
Diego Gomes <diegogns18@outlook.com>
2022-04-27 16:31:02 -0300

    exercicio_classe_pessoa

---
+++ Udemy/exercicio_classes_01/exercicio_classes_01/Nome_idade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio_classes_01
+{
+    class Nome_idade {
+
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ Udemy/exercicio_classes_01/exercicio_classes_01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace exercicio_classes_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nome_idade p1 = new Nome_idade();
+            Nome_idade p2 = new Nome_idade();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if(p1.Idade > p2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            }
+        }
+    }
+}
